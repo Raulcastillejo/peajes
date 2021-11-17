361670397d14c341d8ceb35e3d268f7a5ba7f762
raul castillejo <raulcastillejom@gmail.com>
2021-11-17 08:52:37 -0500

    creacion

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+      </header>
+    </div>
+  );
+}
+}
+export default App;
---
+++ client/src/components/users/Users.js
@@ -0,0 +1,13 @@
+const React=require( 'react');
+ 
+
+class Users extends React.Component {
+  render() {
+    return (
+      React.createElement('h1', 'null','Usuarios en react')
+    );
+  }
+}
+
+module.exports=Users;
+//export default Users;
